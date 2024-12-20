ef40f690a64745e86528a4cc1f99d255e042559d
Gagan <gagandeep1572.be23@chitkara.edu.in>
2024-12-20 21:56:45 +0530

    added file

--- java/3_java_operator/gross_salary_of_an_employee.java
+++ java/3_java_operator/gross_salary_of_an_employee.java
@@ -4,12 +4,13 @@ class Main
 {
   public static void main(String[] args)
   {
+      // Write your code here
       java.util.Scanner sc = new Scanner(System.in);
       int sal = sc.nextInt();
       int HRA = sal *10/100;
       int DA = sal*20/100;
       int totalsal = sal + HRA + DA;
       System.out.println(totalsal);
-    // Write your code here
+      sc.close();
   }
 }
